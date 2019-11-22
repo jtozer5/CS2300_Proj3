eb64b8b9952f8f72ff6d774ebe20c6d06bff10b4
Josh Tozer <joshiantozer@gmail.com>
2019-11-22 14:25:49 -0700

    added table

--- project4.m
+++ project4.m
@@ -1,5 +1,6 @@
 careerYear = [1 2 3 4 5 6 7 8 9 10]';
 incomeUSD = [5000 7500 15000 20000 66000 72000 74500 80000 82000 98000]';
+T = table(careerYear, incomeUSD);
 
 plot(careerYear, incomeUSD, '*')
 
