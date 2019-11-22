56a2d957f7b085e4a1253f33e158cdc341957db7
willsonnathan <55596971+willsonnathan@users.noreply.github.com>
2019-11-22 14:48:48 -0700

    Update project4.m

--- project4.m
+++ project4.m
@@ -2,7 +2,10 @@ careerYear = [1 2 3 4 5 6 7 8 9 10]';
 incomeUSD = [5000 7500 15000 20000 66000 72000 74500 80000 82000 98000]';
 T = table(careerYear, incomeUSD);
 
-plot(careerYear, incomeUSD, '*')
+plot(careerYear, incomeUSD, 'r*')
+title('Cubic least-square regression for Engineer X')
+ylabel('Income (USD)')
+xlabel('Career Year (since 2010)')
 
 [p, S, mu] = polyfit(T.careerYear, T.incomeUSD, 3);
 
